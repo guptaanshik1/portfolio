36f8541bd75130c3e9b89c4f3460dc3a4c3c847e
Anshik Gupta <anshikgupta@Anshiks-MacBook-Pro.local>
2022-08-26 13:28:29 +0530

    Completed the contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,9 +3,18 @@ import React from 'react'
 const Contact = () => {
   return (
     <div name="contact" className='w-full h-screen'>
-        <div className='w-full h-full max-w-[1000px] mx-auto flex justify-center items-center'>
+        <div className='w-full h-full max-w-[1000px] mx-auto flex flex-col justify-center items-center'>
           <div className="text-center pb-8 px-4">
             <p className="sm:text-6xl border-b-4 text-4xl font-bold">Contact</p>
+          </div>
+
+          <div className='px-4 w-full'>
+            <form method="POST" action="https://getform.io/f/a699a1b2-f225-434e-b317-1fbbde8e006c" className="h-auto w-full flex flex-col justify-center items-center">
+              <input className="w-full py-2 text-center mb-2" type="text" name="name" placeholder='Enter name' />
+              <input className="w-full py-2 text-center mb-2" type="email" name="email" placeholder='Enter email' />
+              <textarea className="w-full py-2 text-center" name="message" rows="5" cols="20" placeholder='Enter message'></textarea>
+              <button className="text-white bg-[#8686f0] border-2 rounded-md mt-4 p-4 hover:scale-110 hover:bg-[#fff] hover:text-black hover:border-blue-600 duration-500">Send</button>
+            </form>
           </div>
         </div>
     </div>
